04cebfd58882fd1010b37c7a729b06f88f7abae0
devboell <devboell@gmail.com>
2018-03-12 13:22:12 +0100

    removed unused functions

--- src/lib/fretboard.js
+++ src/lib/fretboard.js
@@ -1,9 +1,5 @@
-export const offset = (pos, nrFrets) =>
-  (1 - (1 / (2 ** (pos / nrFrets)))) * 100 * 2
+/* eslint-disable import/prefer-default-export */
 
 export const width = nrFrets => pos =>
   (((2 ** (1 / nrFrets)) - 1) /
   (2 ** ((pos + 1) / nrFrets))) * 100 * 2
-
-export const position = (pos, nrFrets) =>
-  offset(pos, nrFrets) + width(pos, nrFrets)
