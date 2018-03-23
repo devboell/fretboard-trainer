ebe164bf1be652165efbdb4e596fab8dcca43f7d
devboell <devboell@gmail.com>
2018-03-23 11:00:05 +0100

    handle selection shapes for selected notes and locs

--- src/lib/fretboard.js
+++ src/lib/fretboard.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import { times, sum } from 'ramda'
+import { times, sum, isNil } from 'ramda'
 
 export const fretWidth = nrFrets => pos =>
   (((2 ** (1 / nrFrets)) - 1) /
@@ -16,3 +16,34 @@ export const stringOffset = nrOfStrings => str =>
 
 export const stringCenter = nrOfStrings => str =>
   stringOffset(nrOfStrings)(str) + (stringHeight(nrOfStrings) / 2)
+
+
+export const ensureNoteObjects = noteProps =>
+  noteProps.map(noteProp => (
+    typeof noteProp === 'string'
+      ? ({
+        note: noteProp,
+        status: 'selected',
+        label: noteProp,
+      })
+      : ({
+        note: noteProp.note,
+        status: noteProp.status || 'selected',
+        label: noteProp.label || noteProp.note,
+      })
+  ))
+
+export const ensureLocObjects = locProps =>
+  locProps.map(locProp => (
+    !isNil(locProp.str)
+      ? ({
+        loc: locProp,
+        status: 'selected',
+        label: '',
+      })
+      : ({
+        loc: locProp.loc,
+        status: locProp.status || 'selected',
+        label: locProp.label || '',
+      })
+  ))
