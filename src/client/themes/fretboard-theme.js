246a8ba97b9c7b1845f31ce69f87e7142c014206
devboell <devboell@gmail.com>
2018-03-23 15:46:50 +0100

    split status and octave color maps; better colors for octaves

--- src/client/themes/fretboard-theme.js
+++ src/client/themes/fretboard-theme.js
@@ -1,11 +1,14 @@
 export default {
   background: 'white',
-  highlight: {
-    selected: 'orange',
-    oct2: 'blue',
-    oct3: 'dodgerblue',
-    oct4: 'lightblue',
-    oct5: 'lightgray',
+  statusMap: {
+    selected: 'yellow',
+    unselected: 'white',
+  },
+  octaveMap: {
+    2: 'rgb(95, 190, 244)',
+    3: 'rgb(135, 219, 244)',
+    4: 'rgb(205, 240, 247)',
+    5: 'rgb(242, 246, 247)',
   },
   dimensions: {
     openWidth: 10,
