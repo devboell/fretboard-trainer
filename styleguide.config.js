1a4f6bb5aef2ba681675e757928906240f4333a4
devboell <devboell@gmail.com>
2018-03-26 21:46:08 +0200

    fret component refacor & styleguide config changes

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,10 +1,12 @@
 const path = require('path')
 
 module.exports = {
-  // components: 'src/client/components/**/*.js',
-  components: 'src/client/components/Fretboard/Position/Fret/**/*.js',
+  components: 'src/client/components/Fretboard/Neck/Position/Fret/**/*.js',
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/client/styleguide/ThemeWrapper'),
   },
   skipComponentsWithoutExample: true,
+  getExampleFilename(componentPath) {
+    return componentPath.replace(/index\.jsx?$/, 'Readme.md')
+  },
 }
