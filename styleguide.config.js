666ef04d71b458c7ec2558047edf299add80e9b0
devboell <devboell@gmail.com>
2018-03-27 14:36:47 +0200

    sg config handles index.js files

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,12 +1,15 @@
 const path = require('path')
 
 module.exports = {
-  components: 'src/client/components/Fretboard/Neck/Position/Fret/**/*.js',
+  components: 'src/client/components/**/*.js',
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/client/styleguide/ThemeWrapper'),
   },
   skipComponentsWithoutExample: true,
   getExampleFilename(componentPath) {
-    return componentPath.replace(/index\.jsx?$/, 'Readme.md')
+    const indexRegex = /index\.jsx?$/
+    return componentPath.match(indexRegex)
+      ? componentPath.replace(indexRegex, 'Readme.md')
+      : componentPath.replace(/\.jsx?$/, '.md')
   },
 }
