b30702a8278dfdf71e80f626d6d4b3fd878b1bf0
devboell <devboell@gmail.com>
2018-04-04 17:04:15 +0200

    upgraded react-styleguidist

--- src/client/styleguide/ThemeWrapper.js
+++ src/client/styleguide/ThemeWrapper.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { ThemeProvider } from 'styled-components'
-import theme from 'themes/fretboard-theme'
+// import theme from 'themes/fretboard-theme'
 
+const tmpTheme = {}
 /* eslint-disable react/prefer-stateless-function, react/prop-types */
 export default class ThemeWrapper extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={tmpTheme}>
         {this.props.children}
       </ThemeProvider>
     )
