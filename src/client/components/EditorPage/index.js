77ae5e80b66b7ac3c2a413612283c7d206e62d84
devboell <devboell@gmail.com>
2018-04-24 16:30:56 +0200

    initial commit

---
+++ src/client/components/EditorPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import QuizEditor from 'components/QuizEditor'
+
+
+const EditorPage = () =>
+  <div>
+    Editor Page
+    <QuizEditor />
+  </div>
+
+EditorPage.propTypes = {}
+
+EditorPage.defaultProps = {}
+
+export default EditorPage
