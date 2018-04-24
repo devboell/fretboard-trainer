72d617d694f6d1c00651196fcdb99e91ae53fdf3
devboell <devboell@gmail.com>
2018-04-24 16:19:50 +0200

    start refactor

--- src/client/components/QuizEditor/CreateWrapper.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const CreateWrapper = styled.div`
-  height: 10%;
-`
-
-export default CreateWrapper
--- src/client/components/QuizEditor/FormWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const FormWrapper = styled.div`
-  height: 90%;
-  padding-top: 10px;
-  box-sizing: border-box;
-`
-
-export default FormWrapper
