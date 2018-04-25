9cd5cbc76ab41a67b61a6f38043d57fea2e651b4
devboell <devboell@gmail.com>
2018-04-25 14:48:39 +0200

    removed old code

--- src/client/components/QuizForm/FieldsWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const FieldsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 80%;
-`
-
-export default FieldsWrapper
--- src/client/components/QuizForm/Label.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const Label = styled.label`
-  font-weight: bold;
-`
-
-export default Label
