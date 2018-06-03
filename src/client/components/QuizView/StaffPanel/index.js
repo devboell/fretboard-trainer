22ca65b64d4424b0f97d2e4ceee72ae9917af5df
devboell <devboell@gmail.com>
2018-06-03 18:26:20 +0200

    implemented staff panel

--- src/client/components/QuizView/StaffPanel/index.js
+++ src/client/components/QuizView/StaffPanel/index.js
@@ -1,6 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
+import pt from 'prop-types'
+import vexflow from 'vexflow'
+import { equals } from 'ramda'
+import Wrapper from './Wrapper'
 
-const StaffPanel = () =>
-  <div>Staff Panel</div>
+const VF = vexflow.Flow
+
+class StaffPanel extends Component {
+  componentDidMount() {
+    this.vf = new VF.Factory({
+      renderer: { elementId: this.renderDiv, width: 100, height: 150 },
+    })
+    this.renderNote()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!equals(this.props.entity, prevProps.entity)) {
+      this.clearRenderDiv()
+      this.renderNote()
+    }
+  }
+
+  clearRenderDiv() {
+    const svg = this.renderDiv.firstChild
+    while (svg.hasChildNodes()) {
+      svg.removeChild(svg.lastChild)
+    }
+  }
+
+  renderNote() {
+    const score = this.vf.EasyScore()
+    const system = this.vf.System()
+    const { entity: { notes } } = this.props
+
+    system.addStave({
+      voices: notes.map(note =>
+        score.voice(score.notes(`${note}/w`))),
+    }).addClef('treble')
+
+    this.vf.draw()
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <div
+          ref={(d) => { this.renderDiv = d }}
+        />
+      </Wrapper>
+    )
+  }
+}
+
+StaffPanel.propTypes = {
+  entity: pt.shape({}).isRequired,
+}
+
 
 export default StaffPanel
