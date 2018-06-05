c30589e97359d6bef82bc457bc6f3d0da58b593a
devboell <devboell@gmail.com>
2018-06-05 16:38:26 +0200

    added timer to newQuiz

--- src/client/components/EditorPage/ListEditor/EditorContainer/reducer.js
+++ src/client/components/EditorPage/ListEditor/EditorContainer/reducer.js
@@ -64,6 +64,11 @@ const newQuiz = type => ({
   type,
   tuning: 'standard',
   width: 13,
+  timer: {
+    type: 'none',
+    pause: null,
+    time: null,
+  },
   panelModeIds: ['1'],
   __typename: 'Quiz',
 })
