d38a1ac566cc512c48d4c8bc256ddcf765313659
devboell <devboell@gmail.com>
2018-06-23 08:01:43 +0200

    moved fullscreen to hoc

--- src/client/components/TrainingPage/index.js
+++ src/client/components/TrainingPage/index.js
@@ -3,66 +3,49 @@ import pt from 'prop-types'
 import { compose } from 'ramda'
 import { connect } from 'react-redux'
 import { quizShape } from 'propShapes/quiz'
-
 import Runner from 'components/Runner'
-import { initRunner } from 'components/Runner/reducer' // resetRunner
+import { initRunner, statusMap } from 'components/Runner/reducer'
 import withLoading from 'components/Loading'
 import withData from './enhancers'
-import { toggleModal } from './reducer'
-import StyledModal from './StyledModal'
-import CloseButton from './CloseButton'
-import closeImage from './images/close.png'
 
-const selectQuiz = quiz =>
-  (dispatch) => {
-    dispatch(initRunner(quiz))
-    dispatch(toggleModal())
-  }
-
-const exitQuiz = () =>
-  (dispatch) => {
-    dispatch(toggleModal())
-    // disparch(resetRunner())
-  }
 
 const TrainingPage = ({
   quizzes,
-  showModal,
-  onSelectQuiz,
-  onExitQuiz,
+  status,
+  onInitRunner,
 }) =>
-  <ul>
-    {quizzes.map(quiz =>
-      <li
-        onClick={() => onSelectQuiz(quiz)}
-      >
-        {quiz.name}
-      </li>)
-    }
-    <StyledModal
-      isOpen={showModal}
-    >
-      <CloseButton onClick={() => onExitQuiz()}>
-        <img src={closeImage} alt="Close quiz" />
-      </CloseButton>
+  <div>
+    <ul>
+      {quizzes.map(quiz =>
+        <li key={`quiz-${quiz.id}`}>
+          <button onClick={() => onInitRunner(quiz)}>
+            {quiz.name}
+          </button>
+        </li>)
+      }
+    </ul>
+    {!(status === statusMap.EMPTY) &&
       <Runner />
-    </StyledModal>
-  </ul>
+    }
+  </div>
+
 
 TrainingPage.propTypes = {
   quizzes: pt.arrayOf(quizShape).isRequired,
-  showModal: pt.bool.isRequired,
-  onSelectQuiz: pt.func.isRequired,
-  onExitQuiz: pt.func.isRequired,
+  status: pt.string,
+  onInitRunner: pt.func.isRequired,
+}
+
+TrainingPage.defaultProps = {
+  status: undefined,
 }
 
 const mapStateToProps = state => ({
-  showModal: state.training.showModal,
+  status: state.runner.status,
 })
 
 const mapDispatchToProps = dispatch => ({
-  onSelectQuiz: q => dispatch(selectQuiz(q)),
-  onExitQuiz: () => dispatch(exitQuiz()),
+  onInitRunner: q => dispatch(initRunner(q)),
 })
 
 export default compose(
