da124515dc2a403e00f7af53b440ed857c9d9a5d
devboell <devboell@gmail.com>
2018-06-29 08:55:00 +0200

    added listView reducer

--- src/client/reducers.js
+++ src/client/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
-import list from 'components/EditorPage/ListEditor/ListContainer/reducer'
+import list from 'components/QuizList/reducer'
+import listView from 'components/ListView/reducer'
 import editor from 'components/EditorPage/ListEditor/EditorContainer/reducer'
 import runner from 'components/Runner/reducer'
 
 export default () => combineReducers({
+  listView,
   list,
   editor,
   runner,
