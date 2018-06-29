97d64ce7a2819c5e524be84869614f17dcac525b
devboell <devboell@gmail.com>
2018-06-29 08:54:30 +0200

    made component presentational

--- src/client/components/TrainingPage/index.js
+++ src/client/components/TrainingPage/index.js
@@ -1,55 +1,10 @@
 import React from 'react'
-import pt from 'prop-types'
-import { compose } from 'ramda'
-import { connect } from 'react-redux'
-import { quizShape } from 'propShapes/quiz'
-import Runner from 'components/Runner'
-import { initRunner, statusMap } from 'components/Runner/reducer'
-import withLoading from 'components/Loading'
-import withData from './enhancers'
+import ListView from 'components/ListView'
 
-
-const TrainingPage = ({
-  quizzes,
-  status,
-  onInitRunner,
-}) =>
+const TrainingPage = () =>
   <div>
-    <ul>
-      {quizzes.map(quiz =>
-        <li key={`quiz-${quiz.id}`}>
-          <button onClick={() => onInitRunner(quiz)}>
-            {quiz.name}
-          </button>
-        </li>)
-      }
-    </ul>
-    {!(status === statusMap.EMPTY) &&
-      <Runner />
-    }
+    Training Page
+    <ListView />
   </div>
 
-
-TrainingPage.propTypes = {
-  quizzes: pt.arrayOf(quizShape).isRequired,
-  status: pt.string,
-  onInitRunner: pt.func.isRequired,
-}
-
-TrainingPage.defaultProps = {
-  status: undefined,
-}
-
-const mapStateToProps = state => ({
-  status: state.runner.status,
-})
-
-const mapDispatchToProps = dispatch => ({
-  onInitRunner: q => dispatch(initRunner(q)),
-})
-
-export default compose(
-  withData,
-  connect(mapStateToProps, mapDispatchToProps),
-  withLoading,
-)(TrainingPage)
+export default TrainingPage
