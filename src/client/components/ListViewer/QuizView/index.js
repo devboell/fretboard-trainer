c4b57137f9bab8e33f6bc16c28cbcc1447d00a74
devboell <devboell@gmail.com>
2018-06-29 12:21:12 +0200

    added Runner

--- src/client/components/ListViewer/QuizView/index.js
+++ src/client/components/ListViewer/QuizView/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import pt from 'prop-types'
 import { quizShape } from 'propShapes/quiz'
+import { statusMap } from 'components/Runner/reducer'
+import Runner from 'components/Runner'
 
-const QuizView = ({ quiz, onInitRunner }) =>
+const QuizView = ({ quiz, onInitRunner, runnerStatus }) =>
   <div>
     <button onClick={() => onInitRunner(quiz)}>Train</button>
     <div>{quiz.name}</div>
+    {runnerStatus !== statusMap.EMPTY &&
+      <Runner />
+    }
   </div>
 
 
 QuizView.propTypes = {
   quiz: quizShape.isRequired,
+  runnerStatus: pt.string.isRequired,
   onInitRunner: pt.func.isRequired,
 }
 
