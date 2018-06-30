a0feef09e7e47b1a9c894c395c6287fc140cdecd
devboell <devboell@gmail.com>
2018-06-30 11:52:28 +0200

    use device sizes from css/media

--- src/client/components/QuizViewMedia/index.js
+++ src/client/components/QuizViewMedia/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import 'matchmedia-polyfill'
 import fullScreen from 'components/QuizViewFullscreen'
 import modal from 'components/QuizViewModal'
+import { deviceSizes } from 'css/media'
 
 export default Component =>
   () => {
-    const isPhone = matchMedia('only screen and (max-width: 480px)').matches
+    const phoneQuery = `only screen and (max-width: ${deviceSizes.phone}px)`
+    const isPhone = matchMedia(phoneQuery).matches
     const MediaComponent = isPhone
       ? fullScreen(Component)
       : modal(Component)
