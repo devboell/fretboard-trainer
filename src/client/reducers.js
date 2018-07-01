4b8cbea264e777910bced82b87148cfd28e5ed74
devboell <devboell@gmail.com>
2018-07-01 17:27:04 +0200

    rearrange containers

--- src/client/reducers.js
+++ src/client/reducers.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
-import list from 'components/containers/QuizList/reducer'
 import listViewer from 'components/containers/ListViewer/reducer'
-import editor from 'components/presentational/EditorPage/ListEditor/EditorContainer/reducer'
+import listEditor from 'components/containers/ListEditor/reducer'
 import runner from 'components/containers/Runner/reducer'
 
 export default () => combineReducers({
   listViewer,
-  list,
-  editor,
+  listEditor,
   runner,
 })
