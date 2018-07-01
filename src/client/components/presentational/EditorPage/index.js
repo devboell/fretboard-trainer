b9651de0ffca045f54bd7e0525f16ea382762112
devboell <devboell@gmail.com>
2018-07-01 17:32:31 +0200

    rearrange container components

--- src/client/components/presentational/EditorPage/index.js
+++ src/client/components/presentational/EditorPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import ListEditor from 'components/presentational/EditorPage/ListEditor'
+import ListEditor from 'components/containers/ListEditor'
 
 
 const EditorPage = () =>
