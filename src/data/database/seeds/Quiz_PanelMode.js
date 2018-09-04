a283860d6f2d4357cb7568866c1aac7947af1683
devboell <devboell@gmail.com>
2018-09-04 14:06:53 +0200

    changed texts and config of premade quizzes

--- src/data/database/seeds/Quiz_PanelMode.js
+++ src/data/database/seeds/Quiz_PanelMode.js
@@ -2,10 +2,13 @@ const quizPanelModes = `
   INSERT INTO 'Quiz_PanelMode' VALUES
   (1, 1),
   (2, 1),
-  (2, 2),
-  (2, 3),
   (2, 4),
   (3, 1),
+  (3, 2),
+  (3, 3),
+  (3, 4),
+  (3, 5),
+  (3, 6),
   (4, 1),
   (5, 1),
   (6, 1)
